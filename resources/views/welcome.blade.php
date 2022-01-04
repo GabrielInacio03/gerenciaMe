f42ca76ee8348f699621d32f4b23076b4e7ef937
Gabriel Inacio <inaciogs03@gmail.com>
2022-01-03 21:55:54 -0300

    layout da página inical welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,123 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>GerenciaMe</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
+        
+        <!-- Bootstrap 5.1.3 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+       
+        <!-- Style -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+            .navbar{
+                background: rgba(0,91,224,1);                
             }
-
-            .full-height {
-                height: 100vh;
+            header{
+                background: rgba(0,212,255,1);
             }
-
-            .flex-center {
-                align-items: center;
+            .feature{
+                width: 60px;
+                height: 60px;
                 display: flex;
+                align-items: center;
                 justify-content: center;
             }
-
-            .position-ref {
-                position: relative;
+            i{
+                font-size: 48px; 
             }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+            <div class="container px-5 ">
+                <a class="navbar-brand" href="#!">GerenciaMe</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>                      
+                        <li class="nav-item"><a class="nav-link" href="#!">Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Cadastrar</a></li>
+                    </ul>
                 </div>
             </div>
-        </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-primary py-5">
+            <div class="container px-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-6">
+                        <div class="text-center my-5">
+                            <h1 class="display-5 fw-bolder text-white mb-2">Gerenciador de Cartão de Crédito</h1>
+                            <p class="lead text-white-50 mb-4">Venha conhecer nossa plataforma de gerenciamento de cartão de crédito e nunca mais se perder nas suas contas.</p>
+                            <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
+                                <a class="btn btn-light btn-lg px-4 me-sm-3 " href="#features">Começar</a>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Features section-->
+        <section class="py-5 border-bottom" id="features">
+            <div class="container px-5 my-5">
+                <div class="row gx-5">
+                    <div class="text-center mb-5">
+                        <h2 class="fw-bolder">Vantagens</h2>                        
+                    </div>
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-bar-chart-fill"></i></div>
+                        <h2 class="h4 fw-bolder">Controle</h2>
+                        <p>Um melhor controle das receitas e despesas do seu cartão de crédito, pois você vai poder deixar anotado detalhes e valores.</p>                       
+                    </div>
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
+                        <h2 class="h4 fw-bolder">Balanço Mensal</h2>
+                        <p>Com o balanço Mensal o usuário tem noção se ele está ficando no prejuiso ou não. Assim podendo ter uma vizão geral do que sobrou de receita.</p>                       
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                        <h2 class="h4 fw-bolder">Layout Agrádavel</h2>
+                        <p>O layout do sistema foi pensado para o usuário se sentir confortável em utilizar o sistema tanto no computador quanto em dispositivos móveis.</p>                        
+                    </div>
+                </div>
+            </div>
+        </section>        
+        <!-- Testimonials section-->
+        <section class="bg-light py-5 border-bottom">
+            <div class="container px-5 my-5 px-5">
+                <div class="text-center mb-5">
+                    <h2 class="fw-bolder">Gabriel da Silva Inácio</h2>
+                    <p class="lead mb-0">Autor do Sistema GerenciaMe</p>
+                </div>
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-6">
+                        <!-- Testimonial 1-->
+                        <div class="card mb-4">
+                            <div class="card-body p-4">
+                                <div class="d-flex">
+                                    <div class="flex-shrink-0"><i class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
+                                    <div class="ms-4">
+                                        <p class="mb-1">Desenvolvedor de sistemas Web e cursando tecnologo em Análise e Desenvolvimento de Sistemas na Fatec de Lins.</p>                                       
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                      
+                    </div>
+                </div>
+            </div>
+        </section>       
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; GerenciaMe 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>      
     </body>
 </html>

b4aa8ef5ad5837b62738653013cbdb030f9b715d
Gabriel Inacio <inaciogs03@gmail.com>
2022-01-21 00:23:36 -0300

    ajustes no layout welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,113 +17,68 @@
        
         <!-- Style -->
         <style>
-            
-            .feature{
-                width: 60px;
-                height: 60px;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-            i{
-                font-size: 48px; 
+            .bd-placeholder-img {
+                font-size: 1.125rem;
+                text-anchor: middle;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                user-select: none;
             }
 
+            @media (min-width: 768px) {
+                .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+                }
+            }
+            .nav-link{
+                color: #FFF;
+                font-size: 18px;
+            }
+            .nav-link:hover{    
+                font-weight: bolder;            
+                text-decoration: underline;
+                color: #FFF;
+            }
+            
         </style>
     </head>
-    <body>
-        <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg bg-dark navbar-dark ">
-            <div class="container px-5 ">
-                <a class="navbar-brand" href="#!">GerenciaMe</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <body class="d-flex h-100 text-center text-white bg-dark">       
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto mb-5 flex-column">
+            <header class="mb-auto">
+                <div class="container">
+                    <h3 class="float-md-start mb-0">GerenciaMe</h3>
+                    <nav class="nav nav-masthead justify-content-center float-md-end">
                     @if(Route::has('login'))
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            @auth
-                                <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ url('/Restrito/default') }}">Home</a></li>
-                            @else
-                                <li class="nav-item"><a class="nav-link" href="{{ url('login') }}">Login</a></li>
-
-                                @if(Route::has('register'))
-                                
-                                <li class="nav-item"><a class="nav-link" href="{{ url('register') }}">Cadastrar</a></li>
-                                @endif
-                            @endauth
-                        </ul>
+                        @auth                        
+                            <a class="nav-link active text-light" aria-current="page" href="{{ url('/Restrito/default') }}">Home</a>
+                        @else
+                            @if(Route::has('register'))
+                            <a class="nav-link" href="{{ url('login') }}">Login</a>
+                            <a class="nav-link" href="{{ url('register') }}">Cadastrar</a>
+                            @endif
+                        @endauth
                     @endif
+                    </nav>
                 </div>
-            </div>
-        </nav>
-        <!-- Header-->
-        <header class="bg-dark py-5">
-            <div class="container px-5">
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-6">
-                        <div class="text-center my-5">
-                            <h1 class="display-5 fw-bolder text-white mb-2">Gerenciador de Cartão de Crédito</h1>
-                            <p class="lead text-white-50 mb-4">Venha conhecer nossa plataforma de gerenciamento de cartão de crédito e nunca mais se perder nas suas contas.</p>
-                            <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
-                                <a class="btn btn-light btn-lg px-4 me-sm-3 " href="{{ url('login') }}">Começar</a>                                
-                            </div>
-                        </div>
-                    </div>
+            </header>
+           
+            <main class="px-3">
+                <div class="container">
+                    <h1>Cover your page.</h1>
+                    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                    <p class="lead">
+                        <a href="{{ url('/Restrito/default') }}" type="button" class="btn btn-outline-light btn-lg">Começar</a>
+                    </p>
                 </div>
-            </div>
-        </header>
-        <!-- Features section-->
-        <section class="py-5 border-bottom" id="features">
-            <div class="container px-5 my-5">
-                <div class="row gx-5">
-                    <div class="text-center mb-5">
-                        <h2 class="fw-bolder">Vantagens</h2>                        
-                    </div>
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-bar-chart-fill"></i></div>
-                        <h2 class="h4 fw-bolder">Controle</h2>
-                        <p>Um melhor controle das receitas e despesas do seu cartão de crédito, pois você vai poder deixar anotado detalhes e valores.</p>                       
-                    </div>
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
-                        <h2 class="h4 fw-bolder">Balanço Mensal</h2>
-                        <p>Com o balanço Mensal o usuário tem noção se ele está ficando no prejuiso ou não. Assim podendo ter uma vizão geral do que sobrou de receita.</p>                       
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                        <h2 class="h4 fw-bolder">Layout Agrádavel</h2>
-                        <p>O layout do sistema foi pensado para o usuário se sentir confortável em utilizar o sistema tanto no computador quanto em dispositivos móveis.</p>                        
-                    </div>
+            </main>
+            
+            <footer class="mb-0 m-auto text-white-50" id="rodape">
+                <div class="container">
+                    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white ">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
                 </div>
-            </div>
-        </section>        
-        <!-- Testimonials section-->
-        <section class="bg-light py-5 border-bottom">
-            <div class="container px-5 my-5 px-5">
-                <div class="text-center mb-5">
-                    <h2 class="fw-bolder">Gabriel da Silva Inácio</h2>
-                    <p class="lead mb-0">Autor do Sistema GerenciaMe</p>
-                </div>
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-6">
-                        <!-- Testimonial 1-->
-                        <div class="card mb-4">
-                            <div class="card-body p-4">
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0"><i class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
-                                    <div class="ms-4">
-                                        <p class="mb-1">Desenvolvedor de sistemas Web e cursando tecnologo em Análise e Desenvolvimento de Sistemas na Fatec de Lins.</p>                                       
-                                    </div>
-                                </div>
-                            </div>
-                        </div>                      
-                    </div>
-                </div>
-            </div>
-        </section>       
-        <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; GerenciaMe 2022</p></div>
-        </footer>
+                
+            </footer>
+        </div> 
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>      
     </body>

4d24acd88e9276afe21c86178b4cf009c4e81dcd
Gabriel Inacio <inaciogs03@gmail.com>
2022-02-02 18:38:48 -0300

    layout dashboard alterado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,20 +25,78 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- CSS3 -->
     <link rel="stylesheet" href="{{ URL::asset('css/default.css') }}">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
    <!-- {{ config('app.name', 'GerenciaMe') }} -->
-</head id="page-top">
-<body>   
-    <header>                
-        <h1 id="logo">                
-            <a class="navbar-brand" href="#">Gerencia<sup>Me</sup></a>
-        </h1>            
+</head>
+<body id="page-top">   
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Gerencia<sup>Me</sup></a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>    
+        <div class="navbar-nav">
+            <div class="nav-item text-nowrap" style="display: flex;">
+                <a href="#" class="nav-link px-3">ADMINISTRADOR</a>
+                <a class="nav-link px-3" href="#">Sign out</a>
+            </div>
+        </div>
     </header>
-    <main class="container">
-        @yield('content') 
-    </main>
-   
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">
+                                <span data-feather="home"></span>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">
+                                <span data-feather="credit-card"></span>
+                                Cartões
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">
+                                <span data-feather="arrow-down-circle"></span>
+                                Despesas
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">
+                                <span data-feather="arrow-up-circle"></span>
+                                Receitas
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+
     <!-- Jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="{{ URL::asset('js/dashboard.js') }}"></script>
 </body>
  
 

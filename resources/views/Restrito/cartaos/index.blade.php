372ea6ce65bef014ab6e0ed893b314cd8dde4d2a
Gabriel Inacio <inaciogs03@gmail.com>
2022-02-05 00:00:59 -0300

    ajustando layout da tabela

--- resources/views/Restrito/cartaos/index.blade.php
+++ resources/views/Restrito/cartaos/index.blade.php
@@ -1,45 +1,39 @@
 @extends('layouts.app')
 @section('content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Cartões</h1>
-    <div class="btn-toolbar mb-2 mb-md-0">
-        <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-        </div>
-        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-        <span data-feather="calendar"></span>
-        This week
-        </button>
-    </div>    
+    <h1 class="h2">Cartões</h1>   
 </div>
 <div class="row">
     <div class="col-md-12">
         <a href="{{ url('/Restrito/cartaos/create') }}" class="btn btn-success">Novo</a>
         @if(session()->get('success'))
-        <div class="alert alert-success">
+        <div class="alert alert-success mt-1">
             {{ session()->get('success') }}  
         </div><br />
         @endif
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Nome</th>
-                    <th>Ações</th>
+                    <th class="col-md-1">#</th>
+                    <th class="col-md-9">Nome</th>
+                    <th class="col-md-2">Ações</th>
                 </tr>
             </thead>  
             <tbody>
             @foreach($cartaos as $cartao)
                 <tr>
-                    <td>{{$cartao->id}}</td>
-                    <td>{{$cartao->nome}}</td>                    
-                    <td>
-                        <a href="{{ route('cartaos.edit', $cartao->id)}}" class="btn btn-primary">Editar</a>
+                    <td class="col-md-1">{{$cartao->id}}</td>
+                    <td class="col-md-9">{{$cartao->nome}}</td>                    
+                    <td class="col-md-2">
+                        <a href="{{ route('cartaos.edit', $cartao->id)}}" class="btn btn-primary mb-1">                            
+                            <span data-feather="edit"></span>
+                        </a>
                         <form action="{{ route('cartaos.destroy', $cartao->id)}}" method="post">
                         @csrf
                         @method('DELETE')
-                            <button class="btn btn-danger" type="submit">Deletar</button>
+                            <button class="btn btn-danger" type="submit">
+                                <span data-feather="trash-2"></span>
+                            </button>
                         </form>
                     </td>
                     <td>

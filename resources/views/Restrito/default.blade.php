246fc748bce5f0f86bcd01009d203a8f011c4c3c
GabrielInacio03 <inaciogs03@gmail.com>
2022-05-06 18:45:19 -0300

    testes

--- resources/views/Restrito/default.blade.php
+++ resources/views/Restrito/default.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Dashboard</h1>
-    <p> </p>
+<div class="container-fluid">
+    <div class="row">
+        <h1 class="h2">Dashboard</h1>
+        <p> </p>
+    </div>
 </div>
+
 <div class="row">
     <div class="col-md-12">
         <div id="container-search" class="col-md-12">
